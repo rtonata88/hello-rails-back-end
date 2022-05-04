62bb680cca55e9fa74114b2672e087627cbec3b5
Richard Tonata <rtonata@gmail.com>
2022-05-04 20:56:53 +0200

    Fix linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,9 +1,8 @@
 class GreetingsController < ApplicationController
-  before_action :set_greeting, only: %i[ show edit update destroy ]
+  before_action :set_greeting, only: %i[show edit update destroy]
 
   # GET /greetings or /greetings.json
   def index
     @greetings = Greeting.all
   end
-
 end
